dc33f20cd8bc88efc8a3940d7884c573509e79a4
Jun Young Yoon <jjun9288@gmail.com>
2021-12-23 10:33:29 +0900

    시각

---
+++ 이것이 코딩테스트다/4.구현/4-2.py
@@ -0,0 +1,13 @@
+# 4-2) 시각
+
+n = int(input())
+cnt = 0
+
+for i in range(0, n+1):
+    for j in range(0, 60):
+        for k in range(0, 60):
+            if '3' in str(i) + str(j) + str(k):
+                cnt += 1
+                
+print(cnt)
+            
