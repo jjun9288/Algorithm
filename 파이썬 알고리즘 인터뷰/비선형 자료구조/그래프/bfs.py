0275d90d773d23dfc98d074fe80e489d174e6c84
Jun Young Yoon <jjun9288@gmail.com>
2022-01-26 17:11:47 +0900

    BFS

---
+++ 파이썬 알고리즘 인터뷰/비선형 자료구조/그래프/bfs.py
@@ -0,0 +1,25 @@
+import queue
+
+graph = {
+    1 : [2, 3, 4],
+    2 : [5],
+    3 : [5],
+    4 : [],
+    5 : [6, 7],
+    6 : [],
+    7 : [3]
+}
+
+def bfs_iterative(v):
+    discovered = [v]
+    start = [v]
+    while queue:
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discovered:
+                discovered.append(w)
+                queue.append(w)
+    return discovered
+
+print(bfs_iterative(1))
+        
